e9ddaba72d1a0f8a25950b0f8055d583f9ae92a4
erpriliano <erprilianosaputra.abbas@medirecords.com>
2022-02-04 11:49:47 +0700

    Apply global style to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import GlobalStyles from './globalStyles';
 import { Landing, NotFound } from './pages';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="not-found" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="not-found" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
